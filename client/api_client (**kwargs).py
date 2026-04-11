073315cd39a94d6e36e4691be4144c3a81dbc006
Vitaly Miller <vital.miller@gmail.com>
2026-04-10 22:38:04 -0700

    ✔️

---
+++ client/api_client (**kwargs).py
@@ -0,0 +1,39 @@
+"""
+Base API
+requests.Session()
+"""
+import requests
+from data.data import Base
+
+#=======================================================================================================================
+class APIClient:
+    def __init__(self, base_url: str = Base.URL):
+        self.base_url = base_url
+        self.session = requests.Session()           # 👈Create session
+
+    def request(self, method: str, endpoint: str, **kwargs) -> requests.Response:
+        return self.session.request(                # 👈Send request session
+            method=method,
+            url=f'{self.base_url}{endpoint}',
+            **kwargs
+        )
+
+    # 🟩GET
+    def get(self, endpoint: str, **kwargs) -> requests.Response:
+        return self.request('GET', endpoint, **kwargs)
+
+    # 🟨POST
+    def post(self, endpoint: str, **kwargs) -> requests.Response:
+        return self.request('POST', endpoint, **kwargs)
+
+    # 🟦PUT
+    def put(self, endpoint: str, **kwargs) -> requests.Response:
+        return self.request('PUT', endpoint, **kwargs)
+
+    # 🟪PATCH
+    def patch(self, endpoint: str, **kwargs) -> requests.Response:
+        return self.request('PATCH', endpoint, **kwargs)
+
+    # 🟥DELETE
+    def delete(self, endpoint: str, **kwargs) -> requests.Response:
+        return self.request('DELETE', endpoint, **kwargs)
